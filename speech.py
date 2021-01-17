960122bb90c279776c26a720b9603b497640f797
jeremiasz-goti <jeremiasz.goti@gmail.com>
2021-01-17 23:31:20 +0100

    ignore

--- speech.py
+++ speech.py
@@ -1,23 +1,36 @@
 import time
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
+
 igor_input = input('> ')
+
+# webdriver options
 options = Options()
 options.headless = True
-driver = webdriver.Chrome(r'C:/chromedriver.exe', options=options)
-driver.get('https://www.naturalreaders.com/online/')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument("window-size=1280,800")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
 
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "inputDiv")))
-text_input = driver.find_element(By.ID, 'inputDiv')
+# webdriver init
+driver = webdriver.Chrome(executable_path='/home/heavy_rain/Pulpit/chromedriver', options=options)
+driver.get('https://ttsmp3.com/text-to-speech/Polish/')
+
+
+# input clear input field and put my text
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "voicetext")))
+text_input = driver.find_element(By.ID, 'voicetext')
 text_input.clear()
 text_input.send_keys(igor_input)
 
-driver.find_element(By.ID, 'playBtn').click()
-time.sleep(2)
-driver.find_element(By.XPATH, '//*[@id="playBtn"]').click()
-time.sleep(5)
+# play text
+driver.find_element(By.ID, 'vorlesenbutton').click()
+time.sleep(10)
+
+
 driver.quit()
