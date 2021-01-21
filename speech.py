30228ffdfedc977bd7bfcebdae8c85d2d80dcf3c
jeremiasz-goti <jeremiasz.goti@gmail.com>
2021-01-21 23:14:05 +0100

    basic functions

--- speech.py
+++ speech.py
@@ -7,12 +7,14 @@ from gtts import gTTS
 import playsound
 from random import randint
 
+
 def igor_talk(phrase):
     tts = gTTS(text=phrase, lang='pl')
-    igor_response = r'audio/dynamic/' + (str(randint(0,999))) + '.mp3'
+    igor_response = r'audio/dynamic/' + (str(randint(0, 999))) + '.mp3'
     igor_audio = tts.save(savefile=igor_response)
     playsound.playsound(igor_response)
     os.remove(path=igor_response)
+
 
 def igor_listen():
     try:
@@ -26,27 +28,35 @@ def igor_listen():
         igor_talk('request error')
 
 
+def igor_search(igor_listen):
+    igor_talk('Co wyszukać')
+    search_data = igor_listen()
+    webbrowser.open_new('https://www.google.com/search?q=' + search_data)
+    igor_talk('Zrobione szefie')
+
+
+def igor_weather(igor_listen):
+    igor_talk('Gdzie?')
+    weather_data = igor_listen()
+    weather_key = '8cbfad668c33b1bdce19655af03e5458'
+    weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric&lang=pl'.format(
+        weather_data, weather_key)
+    weather_response = requests.get(weather_url)
+    weather_response_data = json.loads(weather_response.text)
+    weather_description = []
+    for d in weather_response_data["weather"]:
+        weather_description.append(d["description"])
+        igor_talk('Temperatura wynosi {} stopni... a odczuwalna temperatura to {}... warunki panujące na zewnątrz to {}'.format(
+            weather_response_data["main"]["temp"], weather_response_data["main"]["feels_like"], weather_description))
+
+
 # speech recognition
 r = sr.Recognizer()
 
 while True:
     mic_data = igor_listen()
     if 'szukaj' in mic_data:
-        igor_talk('Co wyszukać')
-        search_data = igor_listen()
-        webbrowser.open_new('https://www.google.com/search?q=' + search_data)
-        igor_talk('Zrobione szefie')
+        igor_search(igor_listen)
 
     if 'pogoda' in mic_data:
-        igor_talk('Gdzie?')
-        weather_data = igor_listen()
-        weather_key = '8cbfad668c33b1bdce19655af03e5458'
-        weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric&lang=pl'.format(
-            weather_data, weather_key)
-        weather_response = requests.get(weather_url)
-        weather_response_data = json.loads(weather_response.text)
-        weather_description = []
-        for d in weather_response_data["weather"]:
-            weather_description.append(d["description"])
-        igor_talk('Temperatura wynosi {} stopni... a odczuwalna temperatura to {}... warunki panujące na zewnątrz to {}'.format(
-            weather_response_data["main"]["temp"], weather_response_data["main"]["feels_like"], weather_description))
+        igor_weather(igor_listen)
